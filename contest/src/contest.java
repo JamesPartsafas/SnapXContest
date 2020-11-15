6ed6c6415024a759117b171add01af1aea006ae9
Gawain <60302096+DragonGawain@users.noreply.github.com>
2020-11-15 14:15:25 -0500

    Update contest.java

--- contest/src/contest.java
+++ contest/src/contest.java
@@ -14,29 +14,38 @@ public class contest {
     public static void main(String[] args) {
         JSONParser parser = new JSONParser();
         
-        
-        try (FileReader reader = new FileReader("C:\\Users\\craig\\Downloads\\Training Json-20201115T162845Z-001\\Training_Json\\j4.json")) {
-            Object obj = parser.parse(new FileReader("C:\\Users\\craig\\Downloads\\Training Json-20201115T162845Z-001\\Training_Json\\j4.json"));
-            JSONArray jsonArray = (JSONArray) obj;
-            JSONObject jsonObject = (JSONObject) jsonArray.get(0);
-            Object textAnnotations = jsonObject.get("textAnnotations");
-            String description = textAnnotations.toString().substring(17);
-            System.out.println(description);
-            
-            
+        for (int j = 1; j<21;j++) {
+	        try (FileReader reader = new FileReader("C:\\Users\\craig\\Downloads\\Training Json-20201115T162845Z-001\\Training_Json\\j"+j+".json")) {
+	            Object obj = parser.parse(new FileReader("C:\\Users\\craig\\Downloads\\Training Json-20201115T162845Z-001\\Training_Json\\j"+j+".json"));
+	            JSONArray jsonArray = (JSONArray) obj;
+	            JSONObject jsonObject = (JSONObject) jsonArray.get(0);
+	            Object textAnnotations = jsonObject.get("textAnnotations");
+	            String description = textAnnotations.toString().substring(17);
+	
+	            
+	            String descriptionTwo = description.replace("\\n", " ");
+	            String delimiter = "\\ ";
+	            String[] keywords = descriptionTwo.split(delimiter, 11);
+	            System.out.println("JSON fil nb: "+j);
+	            for (int i = 0; i < 10; i++) {
+	                System.out.println(keywords[i]);
+	            }  
+	            System.out.println();
+	            
+	        }
+	        catch(FileNotFoundException e) 
+	        	{ e.printStackTrace(); 
+	        	}
+	        catch(IOException e) 
+	        	{ e.printStackTrace(); 
+	        	}
+	        catch(ParseException e) 
+	        	{ e.printStackTrace(); 
+	        	}
+	        catch(Exception e) 
+	        	{ e.printStackTrace(); 
+	        	}
         }
-        catch(FileNotFoundException e) 
-        	{ e.printStackTrace(); 
-        	}
-        catch(IOException e) 
-        	{ e.printStackTrace(); 
-        	}
-        catch(ParseException e) 
-        	{ e.printStackTrace(); 
-        	}
-        catch(Exception e) 
-        	{ e.printStackTrace(); 
-        	}
 
     }
 
